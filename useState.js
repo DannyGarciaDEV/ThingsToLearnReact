909cecf9cf2c9f829ff3f0e4fa8c64b5347e9b96
Danny Garcia Cortes <dannygarciadev@gmail.com>
2023-08-30 12:59:04 -0400

    Using useState hook example with my name and age

---
+++ useState.js
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+const Home = () => {
+ 
+    const [name, setName] = useState('danny')
+    const [age, setAge] = useState(20)
+    
+    const handleClick = () => {
+       setName('daniel')
+       setAge(21)
+    }
+    return (
+        <div className="home">
+            <h2> Who is Coding </h2>
+            <p> {name} is {age} </p>
+            <button onclick={handleClick}> Click Me</button>
+        </div>
+    )
+}
+export default Home;
