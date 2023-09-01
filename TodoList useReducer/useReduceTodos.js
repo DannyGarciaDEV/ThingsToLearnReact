12e4aee727fe74532114013a08ecbd9334e4d8f4
Danny Garcia Cortes <dannygarciadev@gmail.com>
2023-09-01 15:49:59 -0400

    adding todoFile to github

---
+++ TodoList useReducer/useReduceTodos.js
@@ -0,0 +1,49 @@
+import React, { useReducer, useState } from 'react';
+import Todo from './Todo.js';
+
+const ACTIONS = {
+  ADD_TODO: 'add-todo',
+  TOGGLE_TODO: 'toggle-todo'
+};
+
+function reducer(todos, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)];
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, complete: !todo.complete };
+        }
+        return todo;
+      });
+    default:
+      return todos;
+  }
+}
+
+function newTodo(name) {
+  return { id: Date.now(), name: name, complete: false };
+}
+
+export default function App() {
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [name, setName] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
+    setName('');
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      </form>
+      {todos.map((todo) => (
+        <Todo key={todo.id} todo={todo} />
+      ))}
+    </>
+  );
+}
